539c00c69a33226879c083ec9ba12e115f7dea0e
Surfskills <ojjfred@gmail.com>
2024-08-06 22:43:13 +0000

    Store front 2nd iteration

--- tooma-ui/src/App.js
+++ tooma-ui/src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Create a custom theme for Material-UI
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#007bff', // Primary color for buttons
+    },
+    secondary: {
+      main: '#0056b3', // Secondary color for hover effects
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif', // Use Roboto font
+  },
+});
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/shared/:uniqueid/:buyerInfoId" element={<LandingPage />} />
-        <Route path="/:username" element={<LandingPage />} />
-        {/* Add other routes here */}
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/shared/:uniqueid/:buyerInfoId" element={<LandingPage />} />
+          <Route path="/:username" element={<LandingPage />} />
+          {/* Add other routes here */}
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
